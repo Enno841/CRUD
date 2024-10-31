5ca6660594ce3d6dc634e732960aa4d85e0a7724
Enno841 <edwincali2002@gmail.com>
2024-10-31 22:43:50 +0800

    Changes to be committed: 	new file:   app/Filament/Resources/CitiesResource.php 	new file:   app/Filament/Resources/CitiesResource/Pages/CreateCities.php 	new file:   app/Filament/Resources/CitiesResource/Pages/EditCities.php 	new file:   app/Filament/Resources/CitiesResource/Pages/ListCities.php 	new file:   app/Filament/Resources/CitiesResource/Pages/ViewCities.php 	new file:   app/Filament/Resources/PersonsResource.php 	new file:   app/Filament/Resources/PersonsResource/Pages/CreatePersons.php 	new file:   app/Filament/Resources/PersonsResource/Pages/EditPersons.php 	new file:   app/Filament/Resources/PersonsResource/Pages/ListPersons.php 	new file:   app/Filament/Resources/PersonsResource/Pages/ViewPersons.php 	new file:   app/Filament/Resources/ProvincesResource.php 	new file:   app/Filament/Resources/ProvincesResource/Pages/CreateProvinces.php 	new file:   app/Filament/Resources/ProvincesResource/Pages/EditProvinces.php 	new file:   app/Filament/Resources/ProvincesResource/Pages/ListProvinces.php 	new file:   app/Filament/Resources/ProvincesResource/Pages/ViewProvinces.php 	new file:   app/Filament/Resources/RegionResource.php 	new file:   app/Filament/Resources/RegionResource/Pages/CreateRegion.php 	new file:   app/Filament/Resources/RegionResource/Pages/EditRegion.php 	new file:   app/Filament/Resources/RegionResource/Pages/ListRegions.php 	new file:   app/Filament/Resources/RegionResource/Pages/ViewRegion.php

---
+++ app/Filament/Resources/PersonsResource.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\PersonsResource\Pages;
+use App\Filament\Resources\PersonsResource\RelationManagers;
+use App\Models\Persons;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class PersonsResource extends Resource
+{
+    protected static ?string $model = Persons::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('region_id')
+                    ->required()
+                    ->numeric(),
+                Forms\Components\TextInput::make('province_id')
+                    ->required()
+                    ->numeric(),
+                Forms\Components\TextInput::make('city_id')
+                    ->required()
+                    ->numeric(),
+                Forms\Components\TextInput::make('first_name')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('last_name')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('middle_name')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('address')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('zip_code')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\DatePicker::make('date_of_birth')
+                    ->required(),
+                Forms\Components\TextInput::make('age')
+                    ->required()
+                    ->numeric(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('region_id')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('province_id')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('city_id')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('first_name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('last_name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('middle_name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('address')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('zip_code')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('date_of_birth')
+                    ->date()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('age')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListPersons::route('/'),
+            'create' => Pages\CreatePersons::route('/create'),
+            'view' => Pages\ViewPersons::route('/{record}'),
+            'edit' => Pages\EditPersons::route('/{record}/edit'),
+        ];
+    }
+}
